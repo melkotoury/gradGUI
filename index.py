69f166e02152b577aa5c67dcafd259064fc53a93
melkotoury <melkotoury@gmail.com>
2016-06-05 21:07:05 +0200

    first gui finished

---
+++ index.py
@@ -0,0 +1,31 @@
+from Tkinter import *
+from PIL import ImageTk, Image
+
+
+app = Tk()
+app.title("DROVEY")
+app.geometry('720x620+200+200')
+
+
+#Methods When Calculate Area is Clicked
+def openFileBrowse():
+    x=2
+
+
+
+welcomeMessage = StringVar()
+welcomeMessage.set("Wecome to DROVEY!")
+lWelcome= Label(app,textvariable=welcomeMessage , height=4)
+lWelcome.pack()
+
+bCalculateArea=Button(app,text="Calculate Area" , bg="orange",fg="white", width=20 , command=openFileBrowse)
+bCalculateArea.pack(padx=15,pady=15,ipadx=10,ipady=10)
+
+img = ImageTk.PhotoImage(Image.open("img/logo.png"))
+panel = Label(app, image = img)
+panel.pack( fill = "both",expand="yes")
+
+
+
+
+app.mainloop()
